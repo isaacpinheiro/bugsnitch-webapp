27333a929bb258b30328ffbf3bd6f33b38cc2ad4
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-04-10 12:58:23 -0300

    Adicionando os templates.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,52 @@
 "use strict";
+
+var app = angular.module('bugsnitch', ['ngRoute', 'ngResource']);
+
+app.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/main.html',
+      controller: 'MainController'
+    })
+    .when('/sobre', {
+      templateUrl: 'templates/sobre.html',
+      controller: 'SobreController'
+    })
+    .when('/equipe', {
+      templateUrl: 'templates/equipe.html',
+      controller: 'EquipeController'
+    })
+    .when('/contato', {
+      templateUrl: 'templates/contato.html',
+      controller: 'ContatoController'
+    })
+    .when('/termos', {
+      templateUrl: 'templates/termos.html',
+      controller: 'TermosController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+}]);
+
+app.controller('MainController', function(){
+
+});
+
+app.controller('SobreController', function(){
+
+});
+
+app.controller('EquipeController', function(){
+
+});
+
+app.controller('ContatoController', function(){
+
+});
+
+app.controller('TermosController', function(){
+
+});
